a41649597d25b99af81cee38abc7d1fc61552e75
Nochvai Maksym <123680608+MaksNochvai@users.noreply.github.com>
2023-04-11 00:49:17 +0300

    Solution ++

--- app/main.py
+++ app/main.py
@@ -27,37 +27,27 @@ class Group:
     course: int
     students: List[Student]
 
-    def get_num_students(self) -> int:
-        return len(self.students)
-
 
 def write_groups_information(group_list: List[Group]) -> int:
     with open("groups.pickle", "wb") as f:
         pickle.dump(group_list, f)
     if group_list:
-        return max(group.get_num_students() for group in group_list)
+        return max(len(group.students) for group in group_list)
 
 
 def write_students_information(student_list: List[Student]) -> int:
     with open("students.pickle", "wb") as f:
         pickle.dump(student_list, f)
-
-    return len(student_list)
+        return len(student_list)
 
 
 def read_groups_information() -> set:
     with open("groups.pickle", "rb") as f:
         groups = pickle.load(f)
-        specialties = set()
-        for group in groups:
-            specialties.add(group.specialty.name)
-        return specialties
+        return set([name.specialty.name for name in groups])
 
 
 def read_students_information() -> list:
     with open("students.pickle", "rb") as f:
-        students = pickle.load(f)
-        students_list = []
-        for student in students:
-            students_list.append(student)
-        return students_list
+        students_data = pickle.load(f)
+        return students_data
