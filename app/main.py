53aedb24fa118dc1a1a936ef3a4491fe9b4d6fdf
S-SMTN <cven28@gmail.com>
2023-06-11 04:07:33 +0300

    implemented Management System

--- app/main.py
+++ app/main.py
@@ -1,1 +1,52 @@
-# write your code here
+import dataclasses
+from datetime import date
+from typing import List
+import pickle
+
+
+@dataclasses.dataclass
+class Specialty:
+    name: str
+    number: int
+
+
+@dataclasses.dataclass
+class Student:
+    first_name: str
+    last_name: str
+    birth_date: date
+    average_mark: float
+    has_scholarship: bool
+    phone_number: str
+    address: str
+
+
+@dataclasses.dataclass
+class Group:
+    specialty: Specialty
+    course: int
+    students: List[Student]
+
+
+def write_groups_information(groups: List[Group]) -> int:
+    with open("groups.pickle", "wb") as f:
+        pickle.dump(groups, f)
+    return max([len(group.students) for group in groups])
+
+
+def write_students_information(students: List[Student]) -> int:
+    with open("students.pickle", "wb") as f:
+        pickle.dump(students, f)
+    return len(students)
+
+
+def read_groups_information() -> List[Specialty]:
+    with open("groups.pickle", "rb") as f:
+        groups = pickle.load(f)
+    return list({group.specialty.name: 0 for group in groups}.keys())
+
+
+def read_students_information() -> List[Student]:
+    with open("students.pickle", "rb") as f:
+        students = pickle.load(f)
+    return students
