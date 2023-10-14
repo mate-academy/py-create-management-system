d5d16149bc733bde21f6ccf7601c2ff97f675344
Vitaliy Perehrest <vit.perehrest@gmail.com>
2023-10-14 11:34:31 +0300

    create_management_system

--- app/main.py
+++ app/main.py
@@ -1,1 +1,66 @@
-# write your code here
+import pickle
+from dataclasses import dataclass
+from datetime import datetime
+from typing import List
+
+
+@dataclass
+class Specialty:
+    name: str
+    number: int
+
+
+@dataclass
+class Student:
+    first_name: str
+    last_name: str
+    birth_date: datetime
+    average_mark: float
+    has_scholarship: bool
+    phone_number: str
+    address: str
+
+
+@dataclass
+class Group:
+    specialty: Specialty
+    course: int
+    students: List[Student]
+
+
+def write_groups_information(groups: List[Group]) -> int:
+    with open("groups.pickle", "wb") as file:
+        pickle.dump(groups, file)
+
+    if not groups:
+        max_students = []
+    else:
+        max_students = max(len(group.students) for group in groups)
+    return max_students
+
+
+def write_students_information(students: List[Student]) -> int:
+    with open("students.pickle", "wb") as file:
+        pickle.dump(students, file)
+
+    return len(students)
+
+
+def read_groups_information() -> list:
+    with open("groups.pickle", "rb") as file:
+        groups = pickle.load(file)
+
+    unique_specialties = set(group.specialty.name for group in groups)
+    return list(unique_specialties)
+
+
+def read_students_information() -> list:
+    students = 0
+    with open("students.pickle", "rb") as file:
+        try:
+            while True:
+                students = pickle.load(file)
+        except EOFError:
+            pass
+
+    return students
