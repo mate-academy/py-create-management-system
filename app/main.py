6a7ee581db7bb7e9acdc04e85a778be70a6e245b
Maksym Operchuk <153509072+MaxOperchuk@users.noreply.github.com>
2024-01-31 17:58:20 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -31,7 +31,10 @@ def write_groups_information(groups: list[Group]) -> int:
     with open("groups.pickle", "wb") as f:
         pickle.dump(groups, f)
     
-    max_group = max((group.students for group in groups if group.students), key=len, default=[])
+    max_group = max(
+        (group.students for group in groups if group.students), 
+        key=len, default=[]
+    )
     return len(max_group)
 
 
