c5c219047bcbcca97e997a3229115c3ca0c5b4ca
Vasyl Kalish <vasyl.documents@yahoo.it>
2024-02-08 22:29:18 +0100

    fix

--- app/main.py
+++ app/main.py
@@ -1,15 +1,15 @@
-import dataclasses
+from dataclasses import dataclass
 from datetime import datetime
 import pickle
 
 
-@dataclasses.dataclass
+@dataclass
 class Specialty:
     name: str
     number: int | float
 
 
-@dataclasses.dataclass
+@dataclass
 class Student:
     first_name: str
     last_name: str
@@ -20,34 +20,35 @@ class Student:
     address: str
 
 
-@dataclasses.dataclass
+@dataclass
 class Group:
     specialty: Specialty
     course: int
     students: list[Student]
 
 
-def write_groups_information(groups: list[Group]):
+def write_groups_information(groups: list[Group]) -> int:
     with open("groups.pickle", "wb") as file:
         pickle.dump(groups, file)
     qty_of_students = 0
     for group in groups:
-        qty_of_students += len(group.students)
+        if qty_of_students < len(group.students):
+            qty_of_students = len(group.students)
     return qty_of_students
 
 
-def write_students_information(students: list[Student]):
+def write_students_information(students: list[Student]) -> int:
     with open("students.pickle", "wb") as file:
         pickle.dump(students, file)
     return len(students)
 
 
-def read_groups_information():
+def read_groups_information() -> set[list]:
     with open("groups.pickle", "rb") as file:
         data = pickle.load(file)
         return set([group.specialty.name for group in data])
 
 
-def read_students_information():
+def read_students_information() -> None:
     with open("students.pickle", "rb") as file:
         return pickle.load(file)
