2ed15375566b382406b81ab7e8edcac7ab6b0982
Sofiia <56766249+sofiazholud@users.noreply.github.com>
2024-02-11 16:40:51 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,14 +1,46 @@
+dimport dataclasses
+from datetime import datetime
+import pickle
+from typing import List
+
+
+@dataclasses.dataclass
+class Specialty:
+    name: str
+    number: int
+
+
+@dataclasses.dataclass
+class Student:
+    first_name: str
+    last_name: str
+    birth_date: datetime
+    average_mark: float
+    has_scholarship: bool
+    phone_number: str
+    address: str
+
+
+@dataclasses.dataclass
+class Group:
+    specialty: Specialty
+    course: int
+    students: List[Student]
+
+
 def write_groups_information(groups: List[Group]) -> int:
     with open("groups.pickle", "wb") as f:
         for group in groups:
             pickle.dump(group, f)
     return max((len(group.students) for group in groups), default=0)
 
+
 def write_students_information(students: List[Student]) -> int:
     with open("students.pickle", "wb") as f:
         for student in students:
             pickle.dump(student, f)
     return len(students)
+
 
 def read_groups_information() -> List[str]:
     groups = []
@@ -19,6 +51,7 @@ def read_groups_information() -> List[str]:
             except EOFError:
                 break
     return list(set(group.specialty.name for group in groups))
+
 
 def read_students_information() -> List[Student]:
     students = []
