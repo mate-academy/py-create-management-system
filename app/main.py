b39fdc569e47e7104e6d9899a13d04b1a076fbb7
dmytro-yefimenko <dmytro@yefimenko.uk>
2024-02-17 17:46:36 +0200

    Create management system: solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,53 @@
-# write your code here
+import pickle
+from datetime import datetime
+from dataclasses import dataclass
+
+
+@dataclass
+class Specialty:
+    name: str
+    number: int
+
+
+@dataclass
+class Student:
+    first_name: str
+    last_name: str
+    birth_date: datetime
+    average_mark: float
+    has_scholarship: bool
+    phone_number: str
+    address: str
+
+
+@dataclass
+class Group:
+    specialty: Specialty
+    course: int
+    students: list[Student]
+
+
+def write_groups_information(groups: list[Group]) -> int:
+    with open("groups.pickle", "wb") as pickle_file:
+        pickle.dump(groups, pickle_file)
+    max_students = max((len(group.students) for group in groups), default=0)
+    return max_students
+
+
+def write_students_information(students: list[Student]) -> int:
+    with open("students.pickle", "wb") as pickle_file:
+        pickle.dump(students, pickle_file)
+    return len(students)
+
+
+def read_groups_information() -> list:
+    with open("groups.pickle", "rb") as pickle_file:
+        groups = pickle.load(pickle_file)
+    unique_specialties = {group.specialty.name for group in groups}
+    return list(unique_specialties)
+
+
+def read_students_information() -> list:
+    with open("students.pickle", "rb") as pickle_file:
+        students = pickle.load(pickle_file)
+    return students
