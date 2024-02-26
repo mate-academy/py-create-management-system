40ceadaec9e53a9d17849e3351754dde166fcaf8
Yuliia Dudych <juliadudych@gmail.com>
2024-02-26 23:07:09 +0200

    Add classes using dataclasses module

--- app/main.py
+++ app/main.py
@@ -1,1 +1,25 @@
-# write your code here
+import dataclasses
+
+
+@dataclasses.dataclass
+class Specialty:
+    name: str
+    number: int
+
+
+@dataclasses.dataclass
+class Student:
+    first_name: str
+    last_name: str
+    birth_date: str
+    average_mark: float
+    has_scholarship: bool
+    phone_number: str
+    address: str
+
+
+@dataclasses.dataclass
+class Group:
+    specialty: Specialty
+    course: int
+    students: list[Student]
