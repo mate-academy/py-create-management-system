fcb12b5d49c8a69b648cebadcd17892e66ab709c
Liliia <furman06228@gmail.com>
2024-04-09 02:48:31 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,69 @@
-# write your code here
+import dataclasses
+import pickle
+from datetime import datetime
+from typing import List, Set
+
+
+@dataclasses.dataclass
+class Specialty:
+    name: str
+    number: int
+
+
+@dataclasses.dataclass
+class Student:
+    first_name: str
+    last_name: str
+    birth_date: datetime
+    average_mark: float
+    has_scholarship: bool
+    phone_number: str
+    address: str
+
+
+@dataclasses.dataclass
+class Group:
+    specialty: Specialty
+    course: int
+    students: List[Student]
+
+
+def write_groups_information(groups: List[Group]) -> int:
+    max_students = 0
+    with open("groups.pickle", "wb") as file:
+        for group in groups:
+            pickle.dump(group, file)
+            if len(group.students) > max_students:
+                max_students = len(group.students)
+    return max_students
+
+
+def write_students_information(students: List[Student]) -> int:
+    with open("students.pickle", "wb") as file:
+        for student in students:
+            pickle.dump(student, file)
+    return len(students)
+
+
+def read_groups_information() -> Set[str]:
+    specialties: Set[str] = set()
+    with open("groups.pickle", "rb") as file:
+        while True:
+            try:
+                group = pickle.load(file)
+                specialties.add(group.specialty.name)
+            except EOFError:
+                break
+    return specialties
+
+
+def read_students_information() -> List[Student]:
+    students: List[Student] = []
+    with open("students.pickle", "rb") as file:
+        while True:
+            try:
+                student = pickle.load(file)
+                students.append(student)
+            except EOFError:
+                break
+    return students
