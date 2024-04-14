8b4374d0a1fa08b4df702289a990cd2aacd25801
Alona Sorochynska <alona.sorochynska@gmail.com>
2024-04-14 21:26:06 +0100

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,39 @@
-# write your code here
+import dataclasses
+import pickle
+
+from datetime import datetime
+
+
+class Specialty:
+    def __init__(self, name: str, number: int) -> None:
+        self.name = name
+        self.number = number
+
+
+class Student:
+    def __init__(
+            self,
+            first_name: str,
+            last_name: str,
+            birth_date,
+            average_mark: float,
+            has_scholarship: bool,
+            phone_number,
+            address: str
+    ) -> None:
+        self.first_name = first_name
+        self.last_name = last_name
+        self.birth_date = birth_date
+        self.average_mark = average_mark
+        self.has_scholarship = has_scholarship
+        self.phone_number = phone_number
+        self.address = address
+
+
+class Group:
+    def __init__(self, specialty: Specialty, course: int, students: list[Student]) -> None:
+        self.specialty = specialty
+        self.course = course
+        self.students = students
+
+
