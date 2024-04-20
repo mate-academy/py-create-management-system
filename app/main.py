fac079463820b80ae5473781faf878fdbbbb8e49
Alona Sorochynska <alona.sorochynska@gmail.com>
2024-04-20 21:06:50 +0100

    solution

--- app/main.py
+++ app/main.py
@@ -1,39 +1,60 @@
-import dataclasses
+import os
 import pickle
-
+from dataclasses import dataclass
 from datetime import datetime
 
 
+@dataclass
 class Specialty:
-    def __init__(self, name: str, number: int) -> None:
-        self.name = name
-        self.number = number
+    name: str
+    number: int
 
 
+@dataclass
 class Student:
-    def __init__(
-            self,
-            first_name: str,
-            last_name: str,
-            birth_date,
-            average_mark: float,
-            has_scholarship: bool,
-            phone_number,
-            address: str
-    ) -> None:
-        self.first_name = first_name
-        self.last_name = last_name
-        self.birth_date = birth_date
-        self.average_mark = average_mark
-        self.has_scholarship = has_scholarship
-        self.phone_number = phone_number
-        self.address = address
+    first_name: str
+    last_name: str
+    birth_date: datetime
+    average_mark: float
+    has_scholarship: bool
+    phone_number: str
+    address: str
 
 
+@dataclass
 class Group:
-    def __init__(self, specialty: Specialty, course: int, students: list[Student]) -> None:
-        self.specialty = specialty
-        self.course = course
-        self.students = students
+    specialty: Specialty
+    course: int
+    students: list[Student]
 
 
+def write_groups_information(groups: list[Group]) -> int:
+    with open("groups.pickle", "wb") as f:
+        pickle.dump(groups, f)
+
+    if not groups:
+        return 0
+
+    return max([len(group.students) for group in groups])
+
+
+def write_students_information(students: list[Student]) -> int:
+    with open("students.pickle", "wb") as f:
+        pickle.dump(students, f)
+
+    return len(students)
+
+
+def read_groups_information() -> list[str]:
+    with open("groups.pickle", "rb") as f:
+        if os.path.getsize("groups.pickle") > 0:
+            specialties = set(
+                group.specialty.name for group in pickle.load(f)
+            )
+    return list(specialties)
+
+
+def read_students_information() -> int:
+    with open("students.pickle", "rb") as f:
+        if os.path.getsize("students.pickle") > 0:
+            return pickle.load(f)
