50b2e12d1ea1cd7779fbe6ba81e922c192638028
Sergiy Yurov <urovsergej10@gmail.com>
2024-04-24 20:41:20 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,7 +2,8 @@ import dataclasses
 
 from datetime import datetime
 
-import  pickle
+import pickle
+
 
 @dataclasses.dataclass
 class Specialty:
@@ -17,7 +18,7 @@ class Student:
     birth_date: datetime
     average_mark: float
     has_scholarship: bool
-    phone: str
+    phone_number: str
     address: str
 
 
@@ -31,11 +32,15 @@ class Group:
 def write_groups_information(list_of_groups: list[Group]) -> int:
     with open("groups.pickle", "wb") as f:
         pickle.dump(list_of_groups, f)
-    students_count = 0
+    students_count = []
     for group in list_of_groups:
-        students_count += len(group.students)
+        students_count.append(len(group.students))
 
-    return students_count
+    if len(students_count) > 1:
+        return max(students_count)
+    elif len(students_count) == 1 and students_count[0] != 0:
+        return students_count[0]
+    return 0
 
 
 def write_students_information(list_of_students: list[Student]) -> int:
@@ -45,7 +50,7 @@ def write_students_information(list_of_students: list[Student]) -> int:
     return len(list_of_students)
 
 
-def read_group_information() -> set:
+def read_groups_information() -> set:
     with open("groups.pickle", "rb") as b:
         groups = pickle.load(b)
 
@@ -60,8 +65,7 @@ def read_group_information() -> set:
 
 
 def read_students_information() -> list[Student]:
-    with open("students.pickle", "rd") as f:
+    with open("students.pickle", "rb") as f:
         student = pickle.load(f)
 
     return student
-
