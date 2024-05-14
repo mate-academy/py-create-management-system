08517e784bd6b6b8e1ede3c145872baecee3c698
Triukhan <tryukhan.d@gmail.com>
2024-05-14 22:11:35 +0300

    triukhan solution

--- app/main.py
+++ app/main.py
@@ -26,41 +26,30 @@ class Group:
     students: list[Student]
 
 
-def write_groups_information(groups: list) -> int:
-    max_stud = 0
-    with open("groups.pickle", "wb") as f:
-        for group in groups:
-            pickle.dump(group, f)
-            if len(group.students) > max_stud:
-                max_stud = len(group.students)
-    return max_stud
+
+
+def write_groups_information(groups: list[Group]) -> int:
+    with open("groups.pickle", "wb") as file:
+        pickle.dump(groups, file)
+
+    return 0 if not groups else (
+        len(max(groups, key=lambda group: len(group.students)).students))
 
 
 def write_students_information(students: list[Student]) -> int:
-    with open("students.pickle", "wb") as f:
-        pickle.dump(students, f)
+    with open("students.pickle", "wb") as file:
+        pickle.dump(students, file)
+
     return len(students)
 
 
 def read_groups_information() -> set:
-    specialties = set()
-    with open("groups.pickle", "rb") as f:
-        try:
-            while True:
-                group = pickle.load(f)
-                specialties.add(group.specialty.name)
-        except EOFError:
-            pass
-    return specialties
+    with open("groups.pickle", "rb") as file:
+        groups = pickle.load(file)
+
+    return {group.specialty.name for group in groups}
 
 
 def read_students_information() -> list:
-    students = []
-    with open("students.pickle", "rb") as f:
-        try:
-            while True:
-                student = pickle.load(f)
-                students.append(student)
-        except EOFError:
-            pass
-    return students[0]
+    with open("students.pickle", "rb") as file:
+        return pickle.load(file)
