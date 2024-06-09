592d1d1742ddfebcaf7f25eada2731f13abe9646
Dmytro Sokolovskiy <167422031+dsokolovskiy@users.noreply.github.com>
2024-06-09 12:58:20 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,45 @@
-# write your code here
+import dataclasses
+from datetime import datetime
+import pickle
+from typing import List, Set
+
+@dataclasses.dataclass
+class Specialty:
+    name: str
+    number: int
+
+@dataclasses.dataclass
+class Student:
+    first_name: str
+    last_name: str
+    birth_date: datetime
+    average_mark: float
+    has_scholarship: bool
+    phone_number: str
+    address: str
+
+@dataclasses.dataclass
+class Group:
+    specialty: Specialty
+    course: int
+    students: List[Student]
+
+def write_groups_information(groups: List[Group]) -> int:
+    with open("groups.pickle", "wb") as file:
+        pickle.dump(groups, file)
+    return max(len(group.students) for group in groups)
+
+def write_students_information(students: List[Student]) -> int:
+    with open("students.pickle", "wb") as file:
+        pickle.dump(students, file)
+    return len(students)
+
+def read_groups_information() -> Set[str]:
+    with open("groups.pickle", "rb") as file:
+        groups = pickle.load(file)
+    return {group.specialty.name for group in groups}
+
+def read_students_information() -> List[Student]:
+    with open("students.pickle", "rb") as file:
+        students = pickle.load(file)
+    return students
