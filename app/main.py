2031e80019cf1eb09b05a81d587fd499a28bb231
Dmytro Sokolovskiy <167422031+dsokolovskiy@users.noreply.github.com>
2024-06-09 13:02:53 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,10 +3,12 @@ from datetime import datetime
 import pickle
 from typing import List, Set
 
+
 @dataclasses.dataclass
 class Specialty:
     name: str
     number: int
+
 
 @dataclasses.dataclass
 class Student:
@@ -18,21 +20,25 @@ class Student:
     phone_number: str
     address: str
 
+
 @dataclasses.dataclass
 class Group:
     specialty: Specialty
     course: int
     students: List[Student]
 
+
 def write_groups_information(groups: List[Group]) -> int:
     with open("groups.pickle", "wb") as file:
         pickle.dump(groups, file)
     return max(len(group.students) for group in groups)
 
+
 def write_students_information(students: List[Student]) -> int:
     with open("students.pickle", "wb") as file:
         pickle.dump(students, file)
     return len(students)
+
 
 def read_groups_information() -> Set[str]:
     with open("groups.pickle", "rb") as file:
