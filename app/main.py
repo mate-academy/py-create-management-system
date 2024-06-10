f4bf5bbd2bd82332c282c46c5331bf6ffa6400e6
TkachukTara <zaz42988@gmail.com>
2024-06-10 15:51:43 +0300

    First try

--- app/main.py
+++ app/main.py
@@ -27,28 +27,31 @@ class Group:
     students: list[Student]
 
 
-def write_groups_information(list_of_group: list[Group]) -> int:
-    with open("groups.pickle" "wb") as file:
-        pickle.dump(list_of_group, file)
-    return max(len(group) for group in list_of_group)
+def write_groups_information(groups: list[Group]) -> int:
+    with open("groups.pickle", "wb") as file:
+        pickle.dump(groups, file)
+    if len(groups) == 0:
+        return 0
+    else:
+        return max(len(group.students) for group in groups)
 
 
-def write_students_information(list_of_student: list[Student]) -> int:
-    with open("students.pickle" "wb") as file:
-        pickle.dump(list_of_student, file)
-    return len(list_of_student)
+def write_students_information(students: list[Student]) -> int:
+    with open("students.pickle", "wb") as file:
+        pickle.dump(students, file)
+    return len(students)
 
 
-def read_groups_information() -> set:
-    retur_set = set()
+def read_groups_information() -> list[str]:
     with open("groups.pickle", "rb") as file:
-        group_information = pickle.load(file)
-    for group in group_information:
-        retur_set.add(group.specialty)
-    return retur_set
+        groups_list = pickle.load(file)
+        result = []
+        for group in groups_list:
+            if group.specialty.name not in result:
+                result.append(group.specialty.name)
+        return result
 
 
-def read_students_information() -> list:
+def read_students_information() -> list[Student]:
     with open("students.pickle", "rb") as file:
-        student_info = pickle.load(file)
-    return student_info
+        return pickle.load(file)
