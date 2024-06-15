0ff81135b30d982ecc65fe4eb4e97bb7d67e9eff
Yevheniia Sosnovska <sosnovskaevgeniya.2504@gmail.com>
2024-06-15 13:08:42 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -29,40 +29,27 @@ class Group:
 
 def write_groups_information(groups: list[Group]) -> int:
     with open("groups.pickle", "wb") as pickle_file:
-        for group in groups:
-            pickle.dump(group, pickle_file)
-
-    max_students = 0
-    if groups:
-        max_students = max(len(group.students) for group in groups)
+        pickle.dump(groups, pickle_file)
+    if not groups:
+        return 0
+    max_students = max(len(group.students) for group in groups)
     return max_students
 
 
 def write_students_information(students: list[Student]) -> int:
     with open("students.pickle", "wb") as pickle_file:
-        for student in students:
-            pickle.dump(student, pickle_file)
-
+        pickle.dump(students, pickle_file)
     return len(students)
 
 
 def read_groups_information() -> list:
-    specialties = set()
-
-    with open("groups.pickle", "rb") as pickle_file:
-        groups = pickle.load(pickle_file)
-        if isinstance(groups, Group):
-            groups = [groups]
-        for group in groups:
-            specialties.add(group.specialty.name)
+    with open("groups.pickle", "rb") as file:
+        groups = pickle.load(file)
+    specialties = {group.specialty.name for group in groups}
     return list(specialties)
 
 
 def read_students_information() -> list:
-
-    with open("students.pickle", "rb") as pickle_file:
-        students = pickle.load(pickle_file)
-
-    if isinstance(students, Student):
-        students = [students]
+    with open("students.pickle", "rb") as file:
+        students = pickle.load(file)
     return students
