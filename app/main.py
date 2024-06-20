1196dbc9486984d949f6ddfd58db1d8cc7a008ed
Maksym Protsak <maksym.protsak@gmail.com>
2024-06-20 21:51:30 +0300

    Solution 20-06-2024 21-51

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from datetime import datetime, date
+from datetime import date
 import pickle
 
 
@@ -28,30 +28,41 @@ class Group:
 
 
 def write_groups_information(group_list: list[Group]) -> int:
+    # if len(group_list) == 0:
+    #     return 0
+
     students_in_group = []
 
     with open("groups.pickle", "wb") as pickle_file:
-        for group in group_list:
-            students_in_group.append(len(group.students))
-        pickle.dump(group, pickle_file)
+        pickle.dump(group_list, pickle_file)
+
+    for group in group_list:
+        students_in_group.append(len(group.students))
+
+    if len(students_in_group) == 0:
+        return 0
 
     return max(students_in_group)
 
 
-def write_students_information():
-    pass
+def write_students_information(students_list: list[Student]) -> int:
+    with open("students.pickle", "wb") as pickle_file:
+        for student in students_list:
+            pickle.dump(student, pickle_file)
+
+    return len(students_list)
 
 
-def read_groups_information():
-    pass
+def read_groups_information() -> list:
+    with open("groups.pickle", "rb") as pickle_file:
+        unique_groups = []
+        data = pickle.load(pickle_file)
+        for group in data:
+            specialty_name = group.specialty.name
+            if specialty_name not in unique_groups:
+                unique_groups.append(specialty_name)
+        return unique_groups
 
 
 def read_students_information():
     pass
-
-
-if __name__ == "__main__":
-    kwargs = [Group(specialty=Specialty("Math and Physic", 1),
-                    course=1,
-                    students=[])]
-    print(write_groups_information(kwargs))
