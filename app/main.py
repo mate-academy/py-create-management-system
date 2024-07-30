1549fb7e5ffbd132ff84fc0808dcb64b3d9662b0
Mikola Rozhansky <bymikola@gmail.com>
2024-07-30 21:21:08 +0300

    1

--- app/main.py
+++ app/main.py
@@ -1,1 +1,29 @@
-# write your code here
+import dataclasses
+from datetime import datetime
+
+
+@dataclasses.dataclass
+class Specialty:
+    name: str
+    number: int
+
+
+@dataclasses.dataclass
+class Student:
+    first_name: str
+    last_name: str
+    birth_date: datetime
+    average_mark: float
+    has_scholarship: bool
+    phone_number: str
+    address: str
+
+
+@dataclasses.dataclass
+class Croup:
+    specialty: Specialty
+    course: int
+    students: list[Student]
+
+
+
