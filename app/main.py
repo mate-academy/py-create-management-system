e66a8cb7981cdc2948ad1508fefb9e121f49c414
Egor Derepovskiy <derepovskiy98@gmail.com>
2024-08-12 00:23:56 +0300

    first commit

--- app/main.py
+++ app/main.py
@@ -38,19 +38,20 @@ def write_groups_information(groups: list[Group]) -> None:
     max_student = max(len(group.students) for group in groups)
     return max_student
 
+
 def write_students_information(students: list[Student]) -> None:
-    with open('students.pickle', 'wb') as handle:
+    with open("students.pickle", "wb") as handle:
         pickle.dump(students, handle)
     return len(students)
 
 
 def read_groups_information() -> list[Group]:
-    with open('groups.pickle', 'rb') as handle:
+    with open("groups.pickle", "rb") as handle:
         groups = pickle.load(handle)
     specialities = {group.speciality.name for group in groups}
     return list(specialities)
 
 def read_students_information() -> list[Student]:
-    with open('students.pickle', 'rb') as handle:
+    with open("students.pickle", "rb") as handle:
         students = pickle.load(handle)
     return students
