b3ba094b0971093497d859ed49a2cff1e877f458
Ivan Shakhman <shakhmanivan@gmail.com>
2024-08-12 16:18:48 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-import dataclasses
 from dataclasses import dataclass
 from datetime import datetime
 import pickle
@@ -30,7 +29,7 @@ class Group:
 
 
 def write_groups_information(groups: List[Group]) -> int:
-    with open('groups.pickle', 'wb') as file:
+    with open("groups.pickle", "wb") as file:
         pickle.dump(groups, file)
     if not groups:
         return 0
@@ -38,19 +37,19 @@ def write_groups_information(groups: List[Group]) -> int:
 
 
 def write_students_information(students: List[Student]) -> int:
-    with open('students.pickle', 'wb') as file:
+    with open("students.pickle", "wb") as file:
         pickle.dump(students, file)
     return len(students)
 
 
 def read_groups_information() -> List[str]:
-    with open('groups.pickle', 'rb') as file:
+    with open("groups.pickle", "rb") as file:
         groups = pickle.load(file)
     specialties = {group.specialty.name for group in groups}
     return list(specialties)
 
 
 def read_students_information() -> List[Student]:
-    with open('students.pickle', 'rb') as file:
+    with open("students.pickle", "rb") as file:
         students = pickle.load(file)
     return students
