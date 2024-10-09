9ffef8e137a67bbfb73ec9f087e83e06bc645835
Max <mxbely98@gmail.com>
2024-10-09 16:18:07 +0300

    created dataclas classes and functions with pickle

--- app/main.py
+++ app/main.py
@@ -1,1 +1,58 @@
-# write your code here
+import pickle
+from dataclasses import dataclass
+from datetime import datetime
+
+
+@dataclass
+class Specialty:
+    name: str
+    number: int
+
+
+@dataclass
+class Student:
+    first_name: str
+    last_name: str
+    birth_date: datetime
+    average_mark: float
+    has_scholarship: bool
+    phone_number: str
+    address: str
+
+
+@dataclass
+class Group:
+    specialty: Specialty
+    course: datetime
+    students: list[Student]
+
+
+def write_groups_information(groups: list[Group]) -> int:
+    with open("groups.pickle", "wb") as file:
+        pickle.dump(groups, file)
+    count = 0
+    for group in groups:
+        if count < len(group.students):
+            count = len(group.students)
+    return count
+
+
+def write_students_information(students: list[Student]) -> int:
+    with open("students.pickle", "wb") as file:
+        pickle.dump(students, file)
+    return len(students)
+
+
+def read_groups_information() -> set:
+    with open("groups.pickle", "rb") as file:
+        groups = pickle.load(file)
+    spec_names = set()
+    for group in groups:
+        spec_names.add(group.specialty.name)
+    return spec_names
+
+
+def read_students_information() -> list:
+    with open("students.pickle", "rb") as file:
+        students = pickle.load(file)
+    return students
