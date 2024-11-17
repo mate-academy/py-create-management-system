1bca024c6801204698b444a192d605e86b814537
Dmytro <labtech.kh@gmail.com>
2024-11-17 07:08:46 +0200

    'Solution_v1.00'

---
+++ app/university_classes.py
@@ -0,0 +1,27 @@
+import dataclasses
+from datetime import datetime
+from typing import List
+
+
+@dataclasses.dataclass
+class Specialty:
+    name: str
+    number: int
+
+
+@dataclasses.dataclass
+class Student:
+    first_name: str
+    last_name: str
+    birth_date: datetime
+    average_mark: float
+    has_scholarship: bool
+    phone_number: str
+    address: str
+
+
+@dataclasses.dataclass
+class Group:
+    specialty: Specialty
+    course: int
+    students: List[Student]
