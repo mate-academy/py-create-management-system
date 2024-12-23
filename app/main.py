fd351f1869e76cd18aca8daf96592efd708ae3fd
Maksym <chukhno.maksym@gmail.com>
2024-12-23 15:39:59 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,69 @@
-# write your code here
+from dataclasses import dataclass
+from datetime import datetime
+import pickle
+
+
+@dataclass
+class Specialty:
+    name: str
+    number: int
+
+
+@dataclass
+class Student:
+    first_name: str
+    last_name: str
+    birth_date: datetime
+    average_mark: float
+    has_scholarship: bool
+    phone_number: int
+    address: str
+
+
+@dataclass
+class Group:
+    specialty: Specialty
+    course: int
+    students: list
+
+
+def write_groups_information(groups: list) -> int:
+    with open("groups.pickle", "wb") as pickle_file:
+        for group in groups:
+            pickle.dump(group, pickle_file)
+
+    return max([len(group.students) for group in groups], default=0)
+
+
+def write_students_information(students: list) -> int:
+    with open("students.pickle", "wb") as file:
+        for student in students:
+            pickle.dump(student, file)
+
+    return len(students)
+
+
+def read_groups_information() -> list:
+    specialty_names = set()
+
+    with open("groups.pickle", "rb") as file:
+        while True:
+            try:
+                group = pickle.load(file)
+                specialty_names.add(group.specialty.name)
+            except EOFError:
+                break
+
+    return list(specialty_names)
+
+
+def read_students_information() -> list:
+    students = []
+    with open("students.pickle", "rb") as file:
+        while True:
+            try:
+                student = pickle.load(file)
+                students.append(student)
+            except EOFError:
+                break
+    return students
