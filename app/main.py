82fe8fb925ff0219956b5bdfab92e694a6b7befb
Oleksandr Barabanov <imtence@icloud.com>
2024-12-25 20:02:26 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -30,35 +30,26 @@ class Group:
 def write_groups_information(groups: list[Group]) -> int:
     with open("groups.pickle", "wb") as file:
         pickle.dump(groups, file)
+    if not groups:
+        return 0
 
-        max_students = max((len(group.students)
-                            for group in groups), default=0)
-        return max_students
+    max_students = max(len(group.students) for group in groups)
+    return max_students
 
 
 def write_students_information(students: list[Student]) -> int:
     with open("students.pickle", "wb") as file:
         pickle.dump(students, file)
-
     return len(students)
 
 
-def read_groups_information() -> list[str]:
-    try:
-        with open("groups.pickle", "rb") as file:
-            groups = pickle.load(file)
-
-        specialties = {group.specialty.name for group in groups}
-        return list(specialties)
-    except FileNotFoundError:
-        return []
+def read_groups_information() -> set[str]:
+    with open("groups.pickle", "rb") as file:
+        groups = pickle.load(file)
+    return {group.specialty.name for group in groups}
 
 
 def read_students_information() -> list[Student]:
-    try:
-        with open("students.pickle", "rb") as file:
-            students = pickle.load(file)
-
-        return students
-    except FileNotFoundError:
-        return []
+    with open("students.pickle", "rb") as file:
+        students = pickle.load(file)
+    return students
