024d2b6865e370c7a64ccc3fd9199b14883502c8
Ron Phillips <mcspidy@gmail.com>
2025-02-02 09:41:22 -0600

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,63 @@
-# write your code here
+import pickle
+from datetime import datetime
+from dataclasses import dataclass
+
+
+@dataclass
+class Specialty:
+    name: str
+    number: int
+
+
+@dataclass
+class Student:
+    first_name: str
+    last_name: str
+    birth_date: datetime
+    average_mark: float
+    has_scholarship: bool
+    phone_number: str
+    address: str
+
+
+@dataclass
+class Group:
+    specialty: Specialty
+    course: list
+    students: list[Student]
+
+
+def write_groups_information(groups: list[Group]) -> int:
+    with open("groups.pickle", "wb") as file:
+        pickle.dump(groups, file)
+
+    max_students = max(len(group.students)
+                       for group in groups) if groups else 0
+
+    return max_students if max_students else 0
+
+
+def write_students_information(students: list[Student]) -> int:
+    with open("students.pickle", "wb") as file:
+        pickle.dump(students, file)
+
+    return len(students) if students else 0
+
+
+def read_groups_information() -> list[Group]:
+    with open("groups.pickle", "rb") as file:
+        groups = pickle.load(file)
+
+        if isinstance(groups, list):
+            groups = list({group.specialty.name
+                           for group in groups
+                           if isinstance(group, Group)})
+
+    return groups if groups else []
+
+
+def read_students_information() -> list[Student]:
+    with open("students.pickle", "rb") as file:
+        students = pickle.load(file)
+
+    return students if isinstance(students, list) else []
