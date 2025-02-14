14f15116cef5d17e4f3dd400f96fa3c60de5a5c1
Svitlana <svetapruta@gmail.com>
2025-02-14 14:14:07 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,61 @@
-# write your code here
+import dataclasses
+from datetime import datetime
+import pickle
+
+
+@dataclasses.dataclass
+class Specialty:
+    name: str
+    number: int
+
+
+@dataclasses.dataclass
+class Student:
+    first_name: str
+    last_name: str
+    birth_date: datetime
+    average_mark: float
+    has_scholarship: bool
+    phone_number: str
+    address: str
+
+
+@dataclasses.dataclass
+class Group:
+    specialty: Specialty
+    course: int
+    students: list[Student]
+
+
+def write_groups_information(groups: list[Group]) -> int:
+    with open("groups.pickle", "wb") as file:
+        pickle.dump(groups, file)
+    for group in groups:
+        if group.students == [] and groups == []:
+            return None
+        return max([len(group.students) for group in groups])
+
+
+def write_students_information(students: list[Student]) -> int:
+    with open("students.pickle", "wb") as file:
+        pickle.dump(students, file)
+    return len(students)
+
+
+def read_groups_information() -> list:
+    try:
+        with open('groups.pickle', 'rb') as file:
+            groups = pickle.load(file)
+        specialties = set(group.specialty.name for group in groups)
+        return list(specialties)
+    except FileNotFoundError:
+        return []
+
+
+def read_students_information() -> list:
+    try:
+        with open('students.pickle', 'rb') as file:
+            students = pickle.load(file)
+        return students
+    except FileNotFoundError:
+        return []
