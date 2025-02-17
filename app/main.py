8e8bef65bca23712984c61bdcc9ada554e73914f
Grigoriy Sventsitskiy <g.gesiod@gmail.com>
2025-02-17 23:24:08 +0200

    Pytest done

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-import datetime
+import os
 import pickle
-from dataclasses import  dataclass
+from dataclasses import dataclass
 from datetime import datetime
+
 
 @dataclass()
 class Specialty:
@@ -14,8 +15,7 @@ class Student:
     first_name: str
     last_name: str
     birth_date: datetime
-    average_mark: int
-    score: int
+    average_mark: float
     has_scholarship: bool
     phone_number: str
     address: str
@@ -26,3 +26,32 @@ class Group:
     specialty: Specialty
     course: int
     students: list[Student]
+
+
+def write_groups_information(groups: list[Group]) -> int:
+    if groups:
+        with open("groups.pickle", "wb") as file:
+            pickle.dump(groups, file)
+        return max(len(group.students) for group in groups)
+    return 0
+
+
+def write_students_information(students: list[Student]) -> int:
+    with open("students.pickle", "wb") as file:
+        pickle.dump(students, file)
+    return len(students)
+
+
+def read_groups_information() -> list[str]:
+    if os.path.exists("groups.pickle"):
+        with open("groups.pickle", "rb") as file:
+            groups = pickle.load(file)
+        specialties = {group.specialty.name for group in groups}
+        return list(specialties)
+    return set()
+
+
+def read_students_information() -> list[Student]:
+    with open("students.pickle", "rb") as file:
+        students = pickle.load(file)
+    return students
