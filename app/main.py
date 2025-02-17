af50ec65732f4f139f8b2d8c46c1af0015f3dd11
Grigoriy Sventsitskiy <g.gesiod@gmail.com>
2025-02-17 03:18:34 +0200

    Create classes

--- app/main.py
+++ app/main.py
@@ -1,1 +1,28 @@
-# write your code here
+import datetime
+import pickle
+from dataclasses import  dataclass
+from datetime import datetime
+
+@dataclass()
+class Specialty:
+    name: str
+    number: int
+
+
+@dataclass()
+class Student
+    first_name: str
+    last_name: str
+    birth_date: datetime
+    average_mark: int
+    score: int
+    has_scholarship: bool
+    phone_number: str
+    address: str
+
+
+@dataclass()
+class Group:
+    specialty: Specialty
+    course: int
+    students: list[Student]
