5e87d2fe9d4d6cac178e41d89d7136264dd28c49
Ngayka <ngayka@gmail.com>
2025-03-06 14:44:25 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -27,27 +27,27 @@ class Group:
     course: int
     students: List[Student]
 
-    @staticmethod
-    def write_groups_information(groups: List["Group"],
-                                 filename: str = "groups.pickle") -> int:
-        with open(filename, "wb") as f:
-            pickle.dump(groups, f)
-        return max((len(group.students) for group in groups), default=0)
 
-    @staticmethod
-    def read_groups_information(filename: str = "groups.pickle") -> list:
-        with open(filename, "rb") as f:
-            groups = pickle.load(f)
-        return list(set(group.speciality.name for group in groups))
+def write_groups_information(groups: List["Group"],
+                             filename: str = "groups.pickle") -> int:
+    with open(filename, "wb") as f:
+        pickle.dump(groups, f)
+    return max((len(group.students) for group in groups), default=0)
 
-    @staticmethod
-    def write_students_information(students: list,
-                                   filename: str = "students.pickle") -> int:
-        with open(filename, "wb") as f:
-            pickle.dump(students, f)
-        return len(students)
 
-    @staticmethod
-    def read_students_information(filename: str = "students.pickle") -> str:
-        with open(filename, "rb") as f:
-            return pickle.load(f)
+def read_groups_information(filename: str = "groups.pickle") -> list:
+    with open(filename, "rb") as f:
+        groups = pickle.load(f)
+    return list(set(group.specialty.name for group in groups))
+
+
+def write_students_information(students: list,
+                               filename: str = "students.pickle") -> int:
+    with open(filename, "wb") as f:
+        pickle.dump(students, f)
+    return len(students)
+
+
+def read_students_information(filename: str = "students.pickle") -> str:
+    with open(filename, "rb") as f:
+        return pickle.load(f)
