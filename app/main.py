fd2a6bbf4c7abf8aee669591267d2c6d42885845
Maksim Pliavinski <pliavinski.maksim@gmail.com>
2025-03-28 13:21:22 +0200

    implement py-create-management-system

--- app/main.py
+++ app/main.py
@@ -1,1 +1,62 @@
-# write your code here
+import dataclasses
+from datetime import datetime
+import os
+import pickle
+
+
+@dataclasses.dataclass
+class Specialty:
+    name: str
+    number: int
+
+
+@dataclasses.dataclass
+class Student:
+    first_name: str
+    last_name: str
+    birth_date: datetime
+    average_mark: float
+    has_scholarship: bool
+    phone_number: str
+    address: str
+
+
+@dataclasses.dataclass
+class Group:
+    specialty: Specialty
+    course: int
+    students: list[Student]
+
+
+def write_groups_information(groups: list[Group]) -> int:
+    with open("groups.pickle", "wb") as f:
+        pickle.dump(groups, f)
+
+    if not groups:
+        return 0
+
+    return max(len(group.students) for group in groups)
+
+
+def write_students_information(students: list[Student]) -> int:
+    with open("students.pickle", "wb") as f:
+        pickle.dump(students, f)
+    return len(students)
+
+
+def read_groups_information() -> list[str]:
+    if not os.path.exists("groups.pickle"):
+        return []
+
+    with open("groups.pickle", "rb") as f:
+        groups = pickle.load(f)
+
+    return list({group.specialty.name for group in groups})
+
+
+def read_students_information() -> list[Student]:
+    if not os.path.exists("students.pickle"):
+        return []
+
+    with open("students.pickle", "rb") as f:
+        return pickle.load(f)
