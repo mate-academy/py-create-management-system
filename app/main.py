f0a331b4adcdac52bb0b25e9835f2b0acc7153a7
Yaroslav Didkivskiy <yaroslavdidkivskiy@gmail.com>
2025-04-08 02:03:07 +0300

    Solution for management system

--- app/main.py
+++ app/main.py
@@ -1,1 +1,55 @@
-# write your code here
+import dataclasses
+import pickle
+
+
+@dataclasses.dataclass
+class Specialty:
+    name: str
+    number: int
+
+
+@dataclasses.dataclass
+class Student:
+    first_name: str
+    last_name: str
+    birth_date: str
+    average_mark: float
+    has_scholarship: bool
+    phone_number: int
+    address: str
+
+
+@dataclasses.dataclass
+class Group:
+    specialty: Specialty
+    course: int
+    students: list[Student]
+
+
+def write_groups_information(groups: list[Group]) -> int:
+    with open("groups.pickle", "wb") as f:
+        pickle.dump(groups, f)
+
+    all_counts = [len(group.students) for group in groups]
+    if not all_counts:
+        return 0
+    return max(all_counts)
+
+
+def write_students_information(students: list[Student]) -> int:
+    with open("students.pickle", "wb") as f:
+        pickle.dump(students, f)
+    return len(students)
+
+
+def read_groups_information() -> list[str]:
+    with open("groups.pickle", "rb") as f:
+        groups = pickle.load(f)
+    specialty_names = {group.specialty.name for group in groups}
+    return list(specialty_names)
+
+
+def read_students_information() -> list[Student]:
+    with open("students.pickle", "rb") as f:
+        students = pickle.load(f)
+        return list(students)
