3ed2ce1ff26d89a32e84d1f52b161d473ea85637
Vitalik <lg.mobile0808@gmail.com>
2025-04-29 20:36:01 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ class Specialty:
     name: str
     number: int
 
+
 @dataclass
 class Student:
     first_name: str
@@ -19,26 +20,31 @@ class Student:
     phone_number: str
     address: str
 
+
 @dataclass
 class Group:
     specialty: Specialty
     course: int
     students: list[Student]
 
+
 def write_groups_information(groups: list[Group]) -> int:
     with open("groups.pickle", "wb") as f:
         pickle.dump(groups, f)
     return max(len(group.students) for group in groups) if groups else 0
+
 
 def write_students_information(students: list[Student]) -> int:
     with open("students.pickle", "wb") as f:
         pickle.dump(students, f)
     return len(students)
 
+
 def read_groups_information() -> set[str]:
     with open("groups.pickle", "rb") as f:
         groups = pickle.load(f)
     return set(group.specialty.name for group in groups)
+
 
 def read_students_information() -> list[Student]:
     with open("students.pickle", "rb") as f:
