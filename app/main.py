f5a7f5af3e8f76f0a7c2dc8518bd22e8b6ac201a
Walery Demidko <walerydz@gmail.com>
2025-06-18 16:24:14 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,66 @@
-# write your code here
+import pickle
+from dataclasses import dataclass
+from datetime import datetime
+
+
+@dataclass
+class Specialty:
+    name: str
+    number: int
+
+
+@dataclass
+class Student:
+    first_name: str
+    last_name: str
+    birth_date: datetime
+    average_mark: float
+    has_scholarship: bool
+    phone_number: str
+    address: str
+
+
+@dataclass
+class Group:
+    specialty: Specialty
+    course: int
+    students: list[Student]
+
+
+def write_groups_information(groups: list[Group]) -> int:
+    with open("groups.pickle", "wb") as f:
+        pickle.dump(groups, f)
+
+    students_numbers = [len(group.students) for group in groups]
+
+    return max(students_numbers) if students_numbers else 0
+
+
+def write_students_information(students: list[Student]) -> int:
+    with open("students.pickle", "wb") as f:
+        for student in students:
+            pickle.dump(student, f)
+
+    return len(students)
+
+
+def read_groups_information() -> list[str]:
+    with open("groups.pickle", "rb") as f:
+        groups = pickle.load(f)
+
+    specialties_names = {group.specialty.name for group in groups}
+
+    return list(specialties_names)
+
+
+def read_students_information() -> list:
+    students = list()
+    with open("students.pickle", "rb") as f:
+        while True:
+            try:
+                student = pickle.load(f)
+                students.append(student)
+            except EOFError:
+                break
+
+    return students
