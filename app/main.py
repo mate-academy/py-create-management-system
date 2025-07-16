78280b613be26b592faa24ea0914bdac0fe91944
Glego <glego200323@gmail.com>
2025-07-16 16:20:17 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,53 @@
-# write your code here
+import dataclasses
+from dataclasses import dataclass
+from datetime import datetime
+import pickle
+
+
+@dataclass
+class Specialty:
+    name: str
+    number: int
+
+
+@dataclass
+class Student:
+    first_name: str
+    last_name: str
+    birth_date: datetime
+    average_mark: float
+    has_scholarship: bool
+    phone_number: str
+    address: str
+
+
+@dataclass
+class Group:
+    specialty: Specialty
+    course: int
+    students: list  # list of Student instances
+
+
+def write_groups_information(groups: list):
+    with open("groups.pickle", "wb") as f:
+        pickle.dump(groups, f)
+    return max((len(group.students) for group in groups), default=0)
+
+
+def write_students_information(students: list):
+    with open("students.pickle", "wb") as f:
+        pickle.dump(students, f)
+    return len(students)
+
+
+def read_groups_information():
+    with open("groups.pickle", "rb") as f:
+        groups = pickle.load(f)
+    specialties = {group.specialty.name for group in groups}
+    return list(specialties)
+
+
+def read_students_information():
+    with open("students.pickle", "rb") as f:
+        students = pickle.load(f)
+    return students
