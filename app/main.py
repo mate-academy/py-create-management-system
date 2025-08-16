c017c050903f260d7809501b7651e6f17f010ce2
Mateusz <szanieckimateusz@gmail.com>
2025-08-16 13:44:29 +0200

    Initial classes implementation

--- app/main.py
+++ app/main.py
@@ -1,1 +1,28 @@
-# write your code here
+import dataclasses
+from datetime import datetime
+from typing import List
+import pickle
+
+
+@dataclasses.dataclass
+class Specialty:
+    name: str
+    number: int
+
+
+@dataclasses.dataclass
+class Student:
+    first_name: str
+    last_name: str
+    birth_date: datetime
+    average_mark: float
+    has_scholarship: bool
+    phone_number: str
+    address: str
+
+
+@dataclasses.dataclass
+class Group:
+    specialty: Specialty
+    course: str
+    students: List[Student]
