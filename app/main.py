f25a0c4893f85e2aff344e2956764019c6b6e68b
Vasyl <vasiliy.azarov.vl1@gmail.com>
2025-09-13 19:51:25 +0200

    Solution #2

--- app/main.py
+++ app/main.py
@@ -28,7 +28,7 @@ class Group:
 
 
 def write_groups_information(groups: list[Group]) -> int:
-    with open('groups.pickle', 'wb') as file:
+    with open("groups.pickle", "wb") as file:
         file.write(pickle.dumps(groups))
     res = 0
     for group in groups:
@@ -38,18 +38,18 @@ def write_groups_information(groups: list[Group]) -> int:
 
 
 def write_students_information(students: list[Student]) -> int:
-    with open('students.pickle', 'wb') as file:
+    with open("students.pickle", "wb") as file:
         file.write(pickle.dumps(students))
     return len(students)
 
 
 def read_groups_information() -> list[str]:
-    with open('groups.pickle', 'rb') as file:
+    with open("groups.pickle", "rb") as file:
         groups = pickle.load(file)
     return list({group.specialty.name for group in groups})
 
 
 def read_students_information() -> list[Student]:
-    with open('students.pickle', 'rb') as file:
+    with open("students.pickle", "rb") as file:
         students = pickle.load(file)
     return students
