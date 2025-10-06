0f6a736cf7b321a2ee3521e7ba80b5e09ecf1ddd
Iago Vinícius Souza de Sales <pristonvinicius@hotmail.com>
2025-10-06 17:05:13 -0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ class Specialty:
 class Student:
     first_name: str
     last_name: str
-    birth_date: str
+    birth_date: datetime
     average_mark: float
     has_scholarship: bool
     phone_number: str
@@ -30,11 +30,12 @@ class Group:
 def write_groups_information(groups: list[Group]) -> int:
     with open("groups.pickle", "wb") as file:
         pickle.dump(groups, file)
-    
+
     number_of_students = 0
 
     for group in groups:
-        number_of_students += len(group.students)
+        if len(group.students) > number_of_students:
+            number_of_students = len(group.students)
 
     return number_of_students
 
@@ -42,15 +43,21 @@ def write_groups_information(groups: list[Group]) -> int:
 def write_students_information(students: list[Student]) -> int:
     with open("students.pickle", "wb") as file:
         pickle.dump(students, file)
-    
+
     return len(students)
 
 
 def read_groups_information() -> list[str]:
+    groups_specialties = []
+    groups: list[Group] = []
+
     with open("groups.pickle", "rb") as file:
-        groups: list[Group] = pickle.load(file)
-    
-    groups_specialties= []
+        while True:
+            try:
+                group_in_pickle: list[Group] = pickle.load(file)
+                groups += group_in_pickle
+            except EOFError:
+                break
 
     for group in groups:
         if group.specialty.name not in groups_specialties:
@@ -60,7 +67,13 @@ def read_groups_information() -> list[str]:
 
 
 def read_students_information() -> list[Student]:
+    students = []
     with open("students.pickle", "rb") as file:
-        students: list[Student] = pickle.load(file)
-    
+        while True:
+            try:
+                student: list[Student] = pickle.load(file)
+                students += student
+            except EOFError:
+                break
+
     return students
