f61959d0ae61a33f563ddaad6b41377fa3d5b368
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2025-10-25 18:35:43 +0200

    implementing classes

--- app/main.py
+++ app/main.py
@@ -1,1 +1,29 @@
-# write your code here
+from dataclasses import dataclass
+from datetime import date
+from typing import List
+
+
+@dataclass
+class Specialty:
+    name: str
+    number: int
+
+
+@dataclass
+class Student:
+    first_name: str
+    last_name: str
+    birth_date: date
+    average_mark: float
+    has_scholarship: bool
+    phone_number: str
+    address: str
+
+
+@dataclass
+class Group:
+    specialty: Specialty
+    course: int
+    students: List[Student]
+
+
