0f06b021366bfce0c667b2e71ed0aa6b60524204
Yevhen Dmytrenko <zdmitrenko26@gmail.com>
2025-11-18 22:43:56 +0200

    add dataclasses

--- app/main.py
+++ app/main.py
@@ -1,5 +1,33 @@
-# write your code here
+import dataclasses
+import pickle
+
+from datetime import datetime
+#from __future__ import annotations
 
 
-def new_func():
+@dataclasses.dataclass
+class Specialty:
+    name: str
+    number: str
+
+
+@dataclasses.dataclass
+class Student:
+    first_name: str
+    last_name: str
+    birth_date: datetime  # TODO: Clarify that!
+    average_mark: float
+    has_scholarship: bool
+    phone_number: str
+    address: str
+
+
+@dataclasses.dataclass
+class Group:
+    specialty: Specialty
+    course: int  # TODO: Clarify that!
+    students: list[Student]
+
+
+def write_groups_information(groups: list[Group]) -> int:
     pass
