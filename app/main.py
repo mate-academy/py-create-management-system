56fe5202653a9b24fefb4fed3a8db1de00c281b3
Tomasz Szatałowicz <szatalowicztomek@gmail.com>
2025-11-29 13:50:21 +0100

    quest

--- app/main.py
+++ app/main.py
@@ -1,53 +1,58 @@
-from dataclasses import dataclass
-from datetime import datetime
+import dataclasses
 import pickle
+from datetime import datetime
 
 
-@dataclass
+@dataclasses.dataclass
 class Specialty:
     name: str
     number: int
 
 
-@dataclass
+@dataclasses.dataclass
 class Student:
     first_name: str
     last_name: str
     birth_date: datetime
-    average_mark : float
-    has_scholarship: str
+    average_mark: float
+    has_scholarship: bool
     phone_number: str
     address: str
 
 
-@dataclass
+@dataclasses.dataclass
 class Group:
     specialty: Specialty
-    course: int
-    students: list
+    course: str
+    students: list[Student]
 
 
-def write_groups_information(groups: list) -> int:
+def write_groups_information(groups: list[Group]) -> int:
     with open("groups.pickle", "wb") as file:
         pickle.dump(groups, file)
-    max_students = max(group.students for group in groups)
-    return max_students
+
+    students_count = []
+    for group in groups:
+        students_count.append(len(group.students))
+    return max(students_count) if students_count else 0
 
 
-def write_students_information(students: list) -> int:
+def write_students_information(students: list[Student]) -> int:
     with open("students.pickle", "wb") as file:
         pickle.dump(students, file)
+
     return len(students)
 
 
-def read_groups_information() -> list:
+def read_groups_information() -> set:
     with open("groups.pickle", "rb") as file:
-        groups = pickle.load(file)
-    specialties = {group.specialty for group in groups}
-    return list(specialties)
+        groups_list = pickle.load(file)
+
+    spec_names = [group.specialty.name for group in groups_list]
+    return set(spec_names)
 
 
 def read_students_information() -> list:
     with open("students.pickle", "rb") as file:
-        students = pickle.load(file)
-    return students
+        students_list = pickle.load(file)
+    return students_list
