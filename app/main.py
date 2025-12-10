70ed51923ef0759b708b34821dca3902a39e01b1
Paweł Dudek <dudek136@gmail.com>
2025-12-10 20:48:24 +0100

    testowanie

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 import dataclasses
 from datetime import datetime
 import pickle
+import os
 
 
 @dataclasses.dataclass
-class Speciality:
+class Specialty:
     name: str
     number: int
 
@@ -17,43 +18,65 @@ class Student:
     average_mark: float
     has_scholarship: bool
     phone_number: str
-    adress: str
+    address: str
 
 
 @dataclasses.dataclass
 class Group:
-    speciality: Speciality
+    specialty: Specialty
     course: int
     students: list[Student]
 
 
 def write_groups_information(list_of_groups: list[Group]) -> int:
     number_of_students = 0
+    if len(list_of_groups) == 0:
+        return 0
+
     for group in list_of_groups:
         number_of_students += len(group.students)
 
-    with open("groups.pickle", "a") as file:
+    # students_list = []
+    # for group in list_of_groups:
+    #     students_list.append(group.students)
+    #
+    # students_list = list(dict.fromkeys(students_list))
+
+    # for student_name_list in students_list:
+    #     student_name_list[0].append
+    # students_list = list(set(students_list))
+    # number_of_students = len(students_list)
+
+    with open("groups.pickle", "wb") as file:
         pickle.dump(list_of_groups, file)
 
     return number_of_students
 
 
 def write_students_information(list_of_students: list[Student]) -> int:
-
-    with open("students.pickle", "a") as file:
+    with open("students.pickle", "wb") as file:
         pickle.dump(list_of_students, file)
 
     return len(list_of_students)
 
 
 def read_groups_information() -> list[Group]:
+    if not os.path.exists("groups.pickle"):
+        return []   # or return [] or return 0 — your choice
+
     with open("groups.pickle", "rb") as file:
         list_of_groups = pickle.load(file)
 
-        list_of_groups = list_of_groups.sort(
-            key=lambda group: group.speciality.name)
+        if list_of_groups is None:
+            return []
 
-        return list_of_groups
+        group_specialites_names = []
+        for group in list_of_groups:
+            group_specialites_names.append(group.specialty.name)
+
+        group_specialites_names = list(set(group_specialites_names))
+
+        return group_specialites_names
 
 
 def read_students_information() -> list[Student]:
